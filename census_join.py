41b0733e811be4835232232f10bdece1f9618062
Amanda Song <amandasong@uchicago.edu>
2026-02-15 23:51:17 -0600

    Wrote join_census_tracts function; still debugging ('field larger than field limit')

--- census_join.py
+++ census_join.py
@@ -4,23 +4,26 @@ from shapely.geometry import Point, Polygon
 from typing import NamedTuple
 
 
-POPULATION_PATH = "data/census/acs_sf_population_2020_24.csv"
+SF_CENSUS_PATH = "data/census/sf_census_tracts_2020.csv"
+POP_PATH = "data/census/acs_sf_population_2020_24.csv"
 RENT_PATH = "data/census/acs_sf_median_rent_2020_24.csv"
-HH_INCOME_PATH = "data/census/acs_sf_median_hh_income_2020_24.csv"
+HH_INC_PATH = "data/census/acs_sf_median_hh_income_2020_24.csv"
 RACE_PATH = "data/census/acs_sf_race_2020_24.csv"
 
-POPULATION_ID = "AUO6E001"
+POP_ID = "AUO6E001"
 RENT_ID = "AUWGE001"
-HH_INCOME_ID = "AURUE001"
+HH_INC_ID = "AURUE001"
 WHITE_POP_ID = "AUO7E002"
 
-
-class CleanedData(NamedTuple):
-    id: str
-    data: int
+KEYS = ["geom", "geo_id", "population", "median_rent", "median_hh_income", "white_pct"]
 
 
-def clean_acs_data(file_path: str, col_name: str) -> list[CleanedData]:
+# class CleanedData(NamedTuple):
+#     id: str
+#     data: int
+
+
+def clean_acs_data(file_path: str, col_name: str) -> dict:
     """
     This function will load the data from the ACS files saved and 
     return a list of CleanedData tuples.
@@ -33,18 +36,21 @@ def clean_acs_data(file_path: str, col_name: str) -> list[CleanedData]:
         col_name: Identifier for the column with the relevant data in that file  
     
     Returns:
-        A list of CleanedData items.
+        A dictionary of GeoID keys that map to their data fields 
+        (None if the original data is a negative value)
     """
-    cleaned_data = []
+    cleaned_data = {}
     
-    with open(Path(__file__).parent / file_path) as f:
+    with open(file_path) as f:
         reader = csv.DictReader(f)
 
         for row in reader:
-            geoid = row["TL_GEO_ID"]
-            data = row[col_name]
+            geo_id = row["TL_GEO_ID"]
+            data = int(row[col_name])
             if data >= 0:
-                cleaned_data.append(CleanedData(geoid, data))
+                cleaned_data[geo_id] = data
+            else: 
+                cleaned_data[geo_id] = None
 
     return cleaned_data
 
@@ -53,4 +59,27 @@ def join_census_tracts():
     """
     Docstring for join_census_tracts
     """
-    pass
+    with open(SF_CENSUS_PATH) as f:
+        reader = csv.DictReader(f)
+
+        with open("census_acs_join.csv", "w") as f:
+            writer = csv.DictWriter(f, KEYS)
+            writer.writeheader()
+
+            for row in reader:
+                geom = row["the_geom"]
+                geo_id = row["geoid"]
+                population = clean_acs_data(POP_PATH, POP_ID)[geo_id]
+                
+                # CHECK IF STARTSWITH WORKS HERE
+                if geom.startswith("MULTIPOLYGON") and geo_id:
+                    writer.writerow({KEYS[0]: geom,
+                                    KEYS[1]: geo_id,
+                                    KEYS[2]: population,
+                                    KEYS[3]: clean_acs_data(RENT_PATH, RENT_ID)[geo_id],
+                                    KEYS[4]: clean_acs_data(HH_INC_PATH, HH_INC_ID)[geo_id],
+                                    KEYS[5]: clean_acs_data(RACE_PATH, WHITE_POP_ID)[geo_id] / population})
+
+
+if __name__ == "__main__":
+    join_census_tracts()
