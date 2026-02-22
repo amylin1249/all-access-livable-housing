0d5fda12f32c3a7c78e119d684d37d899d9f09f6
Amanda Song <amandasong@uchicago.edu>
2026-02-22 14:48:57 -0600

    Combined quadtree_data.py with quadtree.py

--- quadtree.py
+++ quadtree.py
@@ -1,6 +1,46 @@
 from typing import NamedTuple
 from shapely.geometry import Polygon, box, Point
-from .quadtree_data import Location, Tract
+import pathlib
+import shapefile
+
+
+class Location(NamedTuple):
+    id: int
+    latitude: float
+    longitude: float
+### TBD on attributes
+
+class Tract(NamedTuple):
+    id: str
+    pop: int
+    med_rent: int
+    med_hh_inc: int
+    white_pct: float
+    polygon: Polygon
+
+
+def load_shapefiles(path: pathlib.Path) -> list[Tract]:
+    """
+    Extract and parse polygons from census shapefiles.
+    """
+    tracts = []
+    with shapefile.Reader(path) as sf:
+        for shape_rec in sf.shapeRecords():
+            tracts.append(
+                Tract(
+                    id=shape_rec.record["TRACTCE"],
+                    pop=shape_rec.record["population"],
+                    med_rent=shape_rec.record["med_rent"],
+                    med_hh_inc=shape_rec.record["med_hh_inc"],
+                    white_pct=shape_rec.record["white_pct"],
+                    polygon=Polygon(shape_rec.shape.points),
+                )
+            )
+    return tracts
+
+
+### Include code that loads the points data from other sources
+
 
 
 class QuadtreeError(Exception):
