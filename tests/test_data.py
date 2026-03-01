7154ae74f0e52dff2faa68661e1c126264493253
Haeji <ahaeji925@uchicago.edu>
2026-03-01 15:37:07 -0600

    test api

--- tests/test_data.py
+++ tests/test_data.py
@@ -10,9 +10,14 @@ def test_api_fetch():
     assert isinstance(results, list)
     assert len(results)
 
-
-
-
-
+def test_api_conversion():
+    results = get_evictions_data()
+    sample = results[0]
+    assert len(sample) == 4
+    
+    assert isinstance(sample[0], int)
+    assert isinstance(sample[1], float)
+    assert isinstance(sample[2], float)
+    assert isinstance(sample[3], str)
 
 
