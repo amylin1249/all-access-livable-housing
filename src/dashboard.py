536c077f859c5d60267a2a4fa4c8eac46311729e
Amanda Song <amandasong@uchicago.edu>
2026-03-03 15:51:50 -0600

    Made some edits to dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,43 +1,47 @@
-from dash import Dash, html, doc
+from pathlib import Path
+from dash import Dash, html, dcc
+from visualize import create_tract_map, create_scatterplot
+import dash_ag_grid as dag
 import pandas as pd
 import plotly.express as px
-#import dash_ag_grid as dag
+
+
+# Insert path later
+MASTER_DATA = Path()
+
+app = Dash()
+
+# Incorporate data
+df = pd.read_csv(MASTER_DATA)
 
 
 
+# App layout
+app.layout = [
+    html.Div(children="Project All Access Livable Housing"),
+    dag.AgGrid(
+        rowData=df.to_dict("records"),
+        columnDefs=[{"field": i} for i in df.columns]
+    ),
+    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+]
 
 
-#dataframe 
-
-file = pd.DataFrame()
-        pd.read_csv(".csv")
+# # line graph
+# fig = px.line(file, )
 
 
-# line graph
-fig = px.line(file, )
-
-
-
-app = Dash(__name__) 
-
-
-#graph
-app.layout = html.Div([
-        html.H1(),
-        dag.AgGrid(
-            rowData = df.to_dict("")
-            columnDefs = [{"field": i} for i in df.columns]
-        ),
-        dcc.Graph(fifure=fig)
-
-
-
-    ])
+# #graph
+# app.layout = html.Div([
+#         html.H1(),
+#         dag.AgGrid(
+#             rowData = df.to_dict("")
+#             columnDefs = [{"field": i} for i in df.columns]
+#         ),
+#         dcc.Graph(fifure=fig)
+#     ])
 
 
 
-
-#running dash
 if __name__ == '__main__':
     app.run(debug=True)
-
