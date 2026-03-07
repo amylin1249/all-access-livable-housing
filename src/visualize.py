248d62bb5d47495225dd5f78ded3b384ef758dab
Amanda Song <amandasong@uchicago.edu>
2026-03-06 20:18:38 -0600

    Updated choropleth map to make graph objects dynamic

--- src/visualize.py
+++ src/visualize.py
@@ -1,121 +1,38 @@
-import sys
-import shapefile
-import folium
-import webbrowser
 import pandas as pd
 import geopandas as gpd
 import altair as alt
-from altair_saver import save
 import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
-from pyproj import Transformer, CRS
-from shapely import geometry
-from shapely.ops import transform
-<<<<<<< HEAD
-from dash import Dash, html
-import dash_vega_components as dvc
-
-from datatypes import (
-    MERGED_SF_TRACTS_SHP,
-    CONSOLIDATED
-)
-=======
-from datatypes import MERGED_SF_TRACTS_SHP, CONSOLIDATED
->>>>>>> adf2ea0a209d362835d08804117be6b24c50ae73
-
-
-# def visualize_sf_tracts():
-#     """
-#     Visualize SF tracts using MatPlotLib
-#     """
-#     sf_tracts = gpd.read_file(MERGED_SF_TRACTS_SHP)
-#     sf_tracts.plot()
-#     plt.show()
-
-
-# def load_shapefile(
-#     filename: str,
-# ) -> list[tuple]:
-#     """
-#     Load a shapefile using pyshp, returning Shapely geometries.
-#     """
-#     # this will be a list of (geometry, feature_dict) tuples
-#     geometries = []
-
-#     # opens the file (expects a file that ends in .shp)
-#     sf = shapefile.Reader(filename, encodingErrors="replace")
-
-#     fields = [field[0] for field in sf.fields[1:]]
-
-#     for record in sf.shapeRecords():
-#         # a `.shape.__geo_interface__` (essentially a list of points)
-#         # This is passed to the `shapely.shape` function to create
-#         # an instance of the appropriate geometry class.
-#         geom = geometry.shape(record.shape.__geo_interface__)
-
-#         # these objects also have a `.record` attribute
-#         # which is a list-like object with all attributes
-#         # we'll keep the shape in a tuple with its record data
-#         geometries.append((geom, dict(zip(fields, record.record))))
-
-#     return geometries
-
-
-# def quick_map(shapefile_data: list[tuple]):
-#     """
-#     Given shapefile data (returned from load_shapefile)
-#     create a folium map with all geometries for quick visualization.
-#     """
-#     centroid = shapefile_data[0][0].centroid
-#     map = folium.Map(location=[centroid.y, centroid.x], zoom_start=6)
-#     for geom, feat in shapefile_data:
-#         folium.GeoJson(geom.__geo_interface__).add_to(map)
-#     map.save("map.html")
-#     print("created map.html, trying to open browser...")
-#     webbrowser.open("file://" + str(Path.cwd() / "map.html"))
-
-
-# def reproject_geometries(
-#     shapefile_data: list[tuple], from_epsg: str, to_epsg: str
-# ) -> list[tuple]:
-#     """Reproject geometries from one CRS to another."""
-#     transformer = Transformer.from_crs(from_epsg, to_epsg, always_xy=True)
-
-#     reprojected = [
-#         (transform(transformer.transform, geom), feat) for geom, feat in shapefile_data
-#     ]
-#     return reprojected
-
-
-# def get_epsg_from_file(filename: str):
-#     """
-#     This function should read in the text from the PRJ file parameter
-#     and uses
-#     """
-#     with open(filename, "r") as f:
-#         prj_text = f.read()
-#     return CRS.from_wkt(prj_text).to_epsg()
+from datatypes import MERGED_SF_TRACTS_SHP, MERGED
 
 
 def create_tract_map(
-    source_file: Path, start_date: str, end_date: str, col_name: str, agg: str
+    source_file: Path, start_date: str, end_date: str, col_name: str
 ):
     """
     Add docstring
     """
+    METRIC_NAMES = {
+    "311_calls": "311 calls",
+    "eviction_rate": "Eviction rate",
+    "median_rent": "Median rent",
+    "tents": "Tents",
+    "structures": "Structures",
+    "vehicles": "Vehicles",
+    "estimate": "Homelessness estimate",
+    }
+
     df = pd.read_csv(source_file)
     sf_tracts = gpd.read_file(MERGED_SF_TRACTS_SHP)
     ### TBD ON WHETHER THIS SHOULD BE INSIDE OR OUTSIDE FUNCTION
+
     df["tract"] = df["tract"].astype(str).str.zfill(11)
-    df["date"] = pd.to_datetime(df["date"])
-    start_dt = pd.to_datetime(start_date)
-    end_dt = pd.to_datetime(end_date)
-    
+
     filtered_df = (
-        df[(df["date"] >= start_dt) & (df["date"] <= end_dt)]
+        df[(df["date"] >= start_date) & (df["date"] <= end_date)]
         .groupby("tract")
-        .agg(metric=(col_name, agg))
+        .agg(metric=(col_name, "mean"))
         .reset_index()
     )
 
@@ -123,13 +40,13 @@ def create_tract_map(
         alt.Chart(sf_tracts)
         .mark_geoshape()
         .encode(
-            color=alt.Color("metric:Q"),
+            color=alt.Color("metric:Q", title={METRIC_NAMES[col_name]}),
             tooltip=[
-                alt.Tooltip("TL_GEO_ID:N", title="Tract ID"),
+                alt.Tooltip("GEOID:N", title="Tract ID"),
                 alt.Tooltip("population:Q", title="Population"),
                 alt.Tooltip("med_hh_inc:Q", title="Median annual household income"),
                 alt.Tooltip("white_pct:Q", title="% white population"),
-                alt.Tooltip("metric:Q"),
+                alt.Tooltip("metric:Q", title=f"Monthly average {METRIC_NAMES[col_name].lower()}"),
             ],
         )
         .transform_lookup(
@@ -138,8 +55,9 @@ def create_tract_map(
         )
         .project(type="albersUsa")
         .properties(
-            title=f"Unsheltered homelessness in SF tracts ({start_date} to {end_date})"
-        ).interactive()
+            title=f"Average {METRIC_NAMES[col_name].lower()} in SF tracts ({start_date} to {end_date})"
+        )
+        .interactive()
     )
 
     return chart
@@ -186,29 +104,12 @@ def create_scatterplot(
     plt.show()
 
 
-TO_PROJ_EPSG = "EPSG:4326"  # WGS 84 global projection
-
 if __name__ == "__main__":
-    # if len(sys.argv) != 2:
-    #     print("Usage: uv run python visualize.py path/to/shapefile.shp")
-    #     sys.exit(1)
-
-    # filepath = Path(__file__).parent.parent / sys.argv[1]
-
-    # shapefile_data = load_shapefile(filepath)
-    # prj_file = str(filepath).replace("shp", "prj")
-    # from_epsg = get_epsg_from_file(prj_file)
-    # data = reproject_geometries(shapefile_data, from_epsg, TO_PROJ_EPSG)
-
-    # quick_map(data)
-    # uv run python visualize.py clean-data/merged_sf_shapefiles/merged_sf_tracts.shp
-
-    # visualize_sf_tracts()
-    # create_tract_map(CONSOLIDATED, "2020-01", "2024-12", "estimate", "sum")
-    create_scatterplot(
-        CONSOLIDATED,
-        "estimate",
-        "mean",
-        "median_rent",
-        "mean",
-    )
+    create_tract_map(MERGED, "2020-01", "2024-12", "estimate")
+    # create_scatterplot(
+    #     MERGED,
+    #     "estimate",
+    #     "mean",
+    #     "median_rent",
+    #     "mean",
+    # )
